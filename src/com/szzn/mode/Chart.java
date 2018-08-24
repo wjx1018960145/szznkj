5d333b460a4888d2cfbe0509ae5f95a0a5f4fe57
wjx1018960145 <wjx1018960145@163.com>
2018-08-24 19:56:35 +0800

    王军鑫
    王军鑫

---
+++ src/com/szzn/mode/Chart.java
@@ -0,0 +1,16 @@
+package com.szzn.mode;
+
+public class Chart {
+	
+	private String cityName;
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+
+}
